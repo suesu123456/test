63f4e214fab85b229a7a237d403d71665ef5fb43
sue <suesu123456@gmail.com>
2017-01-16 22:46:24 +0800

    【Init】初始化

---
+++ test/test/OCBridgeHeader.h
@@ -0,0 +1,86 @@
+//
+//  OCBridgeHeader.h
+//  Ujiacity
+//
+//  Created by apple on 2016/10/31.
+//  Copyright © 2016年 ujiacity. All rights reserved.
+//
+
+#ifndef OCBridgeHeader_h
+#define OCBridgeHeader_h
+
+#import "AFNetworking+PromiseKit.h"
+/**
+ *  阿里云OSS
+ */
+#import <AliyunOSSiOS/OSSService.h>
+/**
+ *  Umeng share
+ */
+#import <UMSocialCore/UMSocialCore.h>
+#import "UMSocialWechatHandler.h"
+
+/**
+ *  颜色处理
+ */
+#import "UIColor+HexString.h"
+/**
+ *  工具类
+ */
+#import "ZJTools.h"
+/**
+ *  给任何视图添加手势
+ */
+#import "UIView+BlockGesture.h"
+/**
+ *  简写frame
+ */
+#import "UIView+Extension.h"
+/**
+ *  遍历视图
+ */
+#import "UIView+Recursion.h"
+/**
+ *  类似安卓提示框
+ */
+#import "UIView+Toast.h"
+/**
+ *  字体名字大全
+ */
+#import "UIFont+BAKit.h"
+/**
+ *  颜色渐变色
+ */
+#import "UIColor+Gradient.h"
+/**
+ *  随机色
+ */
+#import "UIColor+Random.h"
+/**
+ *  Block形式添加手势点击事件
+ */
+#import "UIButton+Block.h"
+/**
+ *  快速创建控件
+ */
+#import "QuickControl.h"
+/**
+ *  快速创建UImageView及点击事件
+ */
+#import "UIImageView+BlockImageView.h"
+/**
+ *  iOS系统提示框
+ */
+#import "UIViewController+BAAlertView.h"
+/**
+ *  极光推送头文件
+ */
+#import "JPUSHService.h"
+/**
+ *  网络情况监测
+ */
+#import "Reachability.h"
+
+#endif /* OCBridgeHeader_h */
+
+
